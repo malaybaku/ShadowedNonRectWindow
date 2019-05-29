a79728c4f606eaed9846aace2a91e27b9eb58bdb
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-05-29 21:05:53 +0900

    import project

---
+++ ShadowedNonRectWindow/Assets/Baku/ShadowedNonRectWindow/RotateQube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RotateQube : MonoBehaviour
+{
+    private Quaternion _initRot = Quaternion.identity;
+
+    private void Start()
+    {
+        _initRot = transform.rotation;
+    }
+
+    private void Update()
+    {
+        //Rotate by yaw angle
+        transform.rotation = Quaternion.AngleAxis(Time.time * 90, Vector3.up) * _initRot;        
+    }
+}
